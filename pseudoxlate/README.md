4409aef7a2731fcd41badb56098cccd8af7d9fa1
msoutopico <m.soutopico@gmail.com>
2020-09-12 09:33:59 +0200

    New pseudo-translation script

---
+++ pseudoxlate/README.md
@@ -0,0 +1,9 @@
+# OmegaT scripts: Pseudo-translation
+
+This folder contains one Groovy scripts that can be run in OmegaT to accomplish the following tasks:
+
+* Simulate a translation where every letter is translated as x or X (lower and upper case) and numbers are translated as #. Codes such as HTML-like tags and %CODES% are protected.
+
+Future developments:
+- Use other alphabets to simulate translation into Chinese, Arabic, etc.
+- Add expansion or contraction rates, to test whether the translation will fit when there is space limit.
---
+++ pseudoxlate/pseudoxlate.groovy
@@ -0,0 +1,72 @@
+/* :name = Pseudo-translate project :description=
+ * 
+ * @author      Manuel Souto Pico
+ * @date        2020-09-12
+ * @version     0.2.0
+ */
+
+/* 
+ * @versions: 
+ * 0.2.0: added %CODE% exclusion
+ */
+ 
+def gui(){
+	def segm_count = 0;
+
+	project.allEntries.each { currSegm ->
+		editor.gotoEntry(currSegm.entryNum())
+		def target = currSegm.getSrcText();
+
+		// pseudo-translate upper case letters 
+		search = /\p{Lu}(?![^<]*?>|[^%\s]*?%)/   
+		replac = "X"
+		target = target.replaceAll(search, replac)
+
+		// pseudo-translate lower case letters 
+		search = /\p{Ll}(?![^<]*?>|[^%\s]*?%)/   
+		replac = "x"
+		target = target.replaceAll(search, replac)
+
+		// pseudo-translate ditis (in any case ;)
+		search = /\p{N}(?![^<]*?>|[^%\s]*?%)/   
+		replac = "#"
+		target = target.replaceAll(search, replac)
+				
+		segm_count++;
+		console.println(currSegm.entryNum() + ": '" + currSegm.getSrcText() + "' pseudo-translated as '" + target + "'")
+		editor.replaceEditText(target)
+	}
+	console.println(segm_count + " segments modified")
+}
+
+/*
+def gui() {
+	project.allEntries.each { ste ->
+	
+		editor.gotoEntry(ste.entryNum())
+		// copy source 
+		def pseudo = ste.getSrcText()
+		def search
+		def replac
+		// pseudo-translate numbers
+		search = /[0-9](?![^<]*?>)/
+		replac = "#"
+		pseudo = pseudo.replaceAll(search, replac)	
+		// pseudo-translate upper cased letters
+		search = /[A-ZÀÁÄÂÃÈÉËÊÌÍÏÎÒÓÖÔÕÚÙÜÛ](?![^<]*?>)/
+		replac = "X"
+		pseudo = pseudo.replaceAll(search, replac)		
+		// pseudo-translate upper cased letters
+		search = /[a-zàáäâãèéëêìíïîòóöôõùúüû](?![^<]*?>)/
+		replac = "x"
+		pseudo = pseudo.replaceAll(search, replac)		
+		// write pseudo-translation		
+		editor.replaceEditText(pseudo)
+	}
+}
+*/
+
+// Check
+// [a-wyzA-WYZ0-9]
+// [!-’,;.:“”\(\)\]\[\?&=xX#–"º%…—/   ^t^13  ]
+
