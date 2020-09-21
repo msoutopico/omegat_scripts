29db886c4a867a1581de29c4f05bf5b364db457d
msoutopico <m.soutopico@gmail.com>
2020-09-21 21:14:38 +0200

    0.3.0: remote and local scripts and config files based on hash

--- customization/customization.groovy
+++ customization/customization.groovy
@@ -1,6 +1,6 @@
 /* :name = Customization :description =Download langauge assets for a specific container and language pair
  *
- *  @version: 0.2.0
+ *  @version: 0.3.0
  *  @author: Manuel Souto Pico
  */
 
@@ -8,87 +8,60 @@
   *
   *  0.1.0: added hash comparison for all config files
   *  0.2.0: remote and local plugins are compared based on filename
+  *  0.3.0: remote and local scripts and config files based on hash
+  *  0.4.0: domain entered by user or read from properties
   */
 
-
+// user-defined parameter
+domain = "https://cat.capstan.be/OmegaT/custom/"
 
 // first check: is the project being opened?
-import org.omegat.core.events.IProjectEventListener.PROJECT_CHANGE_TYPE
+// import org.omegat.core.events.IProjectEventListener.PROJECT_CHANGE_TYPE
 title = "Customization"
-
 
 // my imports
 import org.apache.commons.io.FilenameUtils
 import groovy.io.FileType
-//
-
-// modules
-import groovy.util.*
-import java.io.File
-import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import org.apache.commons.io.FileUtils
-import java.io.IOException
-import java.util.regex.Matcher
-import java.util.regex.Pattern
-import org.omegat.util.StaticUtils
 import java.security.MessageDigest
+import org.omegat.util.StaticUtils
 import static groovy.io.FileType.FILES
 
-
-import org.omegat.core.events.IApplicationEventListener
-
-import groovy.swing.SwingBuilder
-import groovy.util.XmlSlurper
-//import java.awt.*
-import java.awt.event.WindowAdapter
-import java.awt.event.WindowEvent
-import java.awt.FlowLayout
-import java.awt.GridBagConstraints as GBC
-import java.awt.GridBagLayout as GBL
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.util.zip.ZipFile
-import javax.swing.JLabel
-import javax.swing.JOptionPane
-import javax.swing.JProgressBar
-import javax.swing.WindowConstants
-import org.apache.commons.io.FileUtils
-import org.omegat.CLIParameters
-import org.omegat.util.Preferences
-import org.omegat.util.StaticUtils
-import org.omegat.util.StringUtil
-import static javax.swing.JOptionPane.*
-import static org.omegat.util.Platform.*
-
-// unused
-utils = (StringUtil.getMethods().toString().findAll("makeValidXML")) ? StringUtil : StaticUtils
-
-String.metaClass.confirm = { ->
-    showConfirmDialog null, delegate, title, YES_NO_OPTION
-}
-
-String.metaClass.alert = { ->
-    showMessageDialog null, delegate, title, INFORMATION_MESSAGE
-}
-
+// // modules
+// import groovy.util.*
+// import java.io.File
+// import java.nio.file.Files
+// import org.apache.commons.io.FileUtils
+// import java.io.IOException
+// import java.util.regex.Matcher
+// import java.util.regex.Pattern
+// import org.omegat.core.events.IApplicationEventListener
+// import groovy.swing.SwingBuilder
+// import groovy.util.XmlSlurper
+// //import java.awt.*
+// import java.awt.event.WindowAdapter
+// import java.awt.event.WindowEvent
+// import java.awt.FlowLayout
+// import java.awt.GridBagConstraints as GBC
+// import java.awt.GridBagLayout as GBL
+// import java.nio.file.Files
+// import java.nio.file.Paths
+// import java.util.zip.ZipFile
+// import javax.swing.JLabel
+// import javax.swing.JOptionPane
+// import javax.swing.JProgressBar
+// import javax.swing.WindowConstants
+// import org.apache.commons.io.FileUtils
+// import org.omegat.CLIParameters
+// import org.omegat.util.Preferences
+// import org.omegat.util.StaticUtils
+// import org.omegat.util.StringUtil
+// import static javax.swing.JOptionPane.*
+// import static org.omegat.util.Platform.*
 
 // the script starts here if a project is open
 console.println("="*40 + "\n" + " "*5 + "Customization\n" + "="*40)
-
-// constants
-digest = MessageDigest.getInstance("MD5") // not using def gives the variable global scope
-prop = project.getProjectProperties()
-
-// The container is the first entity in the project name (before the first underscore)
-
-
-
-
-timestamp = new Date().format("YYYYMMddHHmm")
-config_dir = StaticUtils.getConfigDir()
-config_file = new File(config_dir + "containers_config.properties")
 
 // def tgt_code = 			project.projectProperties.targetLanguage
 // def src_code = 			project.projectProperties.sourceLanguage
@@ -97,50 +70,9 @@ config_file = new File(config_dir + "containers_config.properties")
 // def tgt_region_subtag = project.projectProperties.targetLanguage.countryCode
 // def src_region_subtag = project.projectProperties.sourceLanguage.countryCode
 
-// https://www.baeldung.com/groovy-file-read
-String read_file_string(String path_to_file) {
-    File file = new File(path_to_file)
-    String file_content = file.text
-    return file_content
-}
-
-def download_asset(urlstr_to_remote_asset, dest_dir) {
-
-	URL url = new URL(urlstr_to_remote_asset)
-	remote_asset_name = FilenameUtils.getName(url.getPath()) // -> file.xml
-	// console.println(FilenameUtils.getBaseName(url.getPath())); // -> file
-	// console.println(FilenameUtils.getExtension(url.getPath())); // -> xml
-	console.println("--\nDOWNLOAD ${remote_asset_name} from ${urlstr_to_remote_asset}.")
-
-	try {
-		// def url = domain + "files/" + remote_asset_name
-		def dest_file = new File(dest_dir.toString() + File.separator + remote_asset_name)
-		dest_file.withOutputStream { output_stream  ->
-
-			// if url exists??? jarx
-
-			// def url = new URL(urlstr_to_remote_asset).openConnection()
-			def conn = url.openConnection()
-			output_stream << conn.inputStream
-			// FileUtils.copyInputStreamToFile(url.toURL().openStream(), dest_file)
-		}
-	} catch (IOException e) {
-		// unable to download asset
-		console.println("Unable to download asset: " + e.message)
-		return
-	}
-}
-
-
-
-
-
-
-
 def get_local_hash_list(dest, remote_file_list) {
 
 	// assert dest == config_dir
-
 	def local_config_dir = new File(dest) // either glossary_dir or tm_dir
 	def local_file_hash_map = [:]
 
@@ -159,7 +91,7 @@ def get_local_hash_list(dest, remote_file_list) {
 		if (remote_file_list.contains(unix_rel_path)) {
 			// https://128bit.io/2011/02/17/md5-hashing-in-python-ruby-and-groovy/
 			def asset_hash = new BigInteger(1,digest.digest(it.getBytes())).toString(16).padLeft(32,"0")
-			console.println(asset_hash + " <= " + unix_rel_path)
+			// console.println(asset_hash + " <= " + unix_rel_path)
 			local_file_hash_map.put(unix_rel_path, asset_hash)
 		}
 	}
@@ -175,45 +107,108 @@ def fetch_hash_list(url_to_hash_list) {
 		return url.inputStream.readLines()
 	}  catch (IOException e) {
 		// last_modif will stay an empty array
-		console.println("Unable to download hash list: " + e.message)
+		message.add("!! Unable to download hash list: " + e.message)
 		return // stop script if list of hashes not available?? or just download everything found?
 	}
 }
 
-def update_assets(domain, dest) {
+def download_asset(urlstr_to_remote_file) {
 
-	def local_config_dir = new File(dest)
-	def remote_config_dir = domain + "config/"
+	URL url = new URL(urlstr_to_remote_file)
+	def remote_file_name = FilenameUtils.getName(url.getPath()) // -> file.xml
+	def rel_path_str_to_dest = urlstr_to_remote_file.minus(domain + "config/").toString()
 
-	url_to_hash_list = domain + hash_filename
-	hash_list = fetch_hash_list(url_to_hash_list)
+	def dest = config_dir + rel_path_str_to_dest
+	def local_path_to_dest = ( System.properties['os.name'].toLowerCase().contains('windows') ? FilenameUtils.separatorsToWindows(dest) : dest )
 
-	if (!hash_list) {
-		console.println("No hash list found, unable to continue.")
+	message.add(">> DOWNLOAD " + remote_file_name + " to " + rel_path_str_to_dest)
+
+	try {
+		// def url = domain + "files/" + remote_file_name
+		def dest_file = new File(local_path_to_dest.toString())
+		dest_file.withOutputStream { output_stream  ->
+
+			def conn = url.openConnection()
+			output_stream << conn.inputStream
+		}
+	} catch (IOException e) {
+		message.add("!! Unable to download file: " + e.message)
 		return
 	}
+}
 
-	hash_list.each { it ->
-		console.println("line in hash list: " + it)
+def delete_old_plugins(new_jar_relpath, plugins_dir) {
+
+	// URL url = new URL(urlstr_to_remote_file)
+	new_jar_name = FilenameUtils.getName(new_jar_relpath)
+	jar_files = new_jar_name.minus(~/-\d+.*\.jar$/) + "*.jar"
+	// console.println("\nI will delete files with pattern " + jar_files)
+	def files_to_delete = new FileNameFinder().getFileNames(plugins_dir.toString(), jar_files /* includes */, new_jar_name /* excludes */)
+
+	files_to_delete.each { it -> // it is a path
+
+		def file = new File(it)
+		file.delete() // on Windows this might not work if OmegaT is running>
+		message.add("--- DELETE (at least try) $new_jar_name")
+		// // some black magic to byass window's lock?
 	}
+}
 
-
-
-
-
-	// remove plugins
-	// hash_list = hash_list.findAll { !it.contains(":plugins/") }
-
-
-
-	hash_list.each { it ->
-		console.println("line in hash list without plugins: " + it)
+def fetch_plugins_by_name(local_file_hash_map, remote_plugins) {
+	// fetch plugins' jar files (comparing remote to local filenames)
+	remote_plugins.each { plugin ->
+		// console.println(it)
+		def downloaded = local_file_hash_map.containsKey(plugin)
+		if (!downloaded) {
+			delete_old_plugins(plugin, plugins_dir)
+			download_asset(remote_config_dir + plugin)
+			message.add("++ Remote custom file " + plugin + " downloaded to " + plugins_dir)
+		} else {
+			// strange output if this line is not commented...
+			// message.add("== Remote custom file " + plugin + " is already up to date")
+		}
 	}
+}
 
-	// put remote list of hashes in array (filename -> hash)
-	//
-	// Pattern re = ~/${container}_(?:.*?)_Glossary_${tgt_code}/
-	// def match = re.matcher(str).asBoolean()
+def fetch_files_by_hash(local_file_hash_map, remote_file_hash_map) {
+	// get other custom files (compare remote to local hashes)
+	remote_file_hash_map.each {
+		remote_file_name = it.key
+		remote_file_hash = it.value
+
+		// if the remote asset is found in the assets folder
+		def downloaded = local_file_hash_map.containsKey(remote_file_name)
+		if (downloaded) {
+			def local_asset_hash = local_file_hash_map.find{ it.key == remote_file_name }?.value
+			if (local_asset_hash) {
+				if (local_asset_hash == remote_file_hash) {
+					// message.add("== Remote custom file ${remote_file_name} hasn't changed, the local copy is up to date.")
+				} else {
+					message.add("++ Remote custom file " + remote_file_name + " has been updated, downloading now.")
+					// download
+					download_asset(remote_config_dir + remote_file_name)
+				}
+			}
+		} else {
+			// download
+			// check if it exists locally, if it does, then msg:
+			message.add("++ Remote custom file " + remote_file_name + " is new or had not been downloaded, downloading now.")
+			// download
+			download_asset(remote_config_dir + remote_file_name)
+		}
+	}
+}
+
+def update_assets() {
+
+	// def local_config_dir = new File(dest)
+	def url_to_hash_list = domain + hash_filename
+	def hash_list = fetch_hash_list(url_to_hash_list)
+
+	if (!hash_list) {
+		message.add("!!! No hash list found, unable to continue.")
+		return
+	}
 
 	// moveing the list to a map
 	def remote_file_hash_map = hash_list.collectEntries {
@@ -221,137 +216,38 @@ def update_assets(domain, dest) {
 		def file = it.split(':')[1]
 		[(file): hash]
 	}
-	remote_file_hash_map.each {
-		// console.println("line in hash map: " + it.key + ":" + it.value)
-	}
-
 
 	// get local assets and their hashes
-	local_file_hash_map = get_local_hash_list(dest, remote_file_hash_map.keySet())
+	def local_file_hash_map = get_local_hash_list(config_dir, remote_file_hash_map.keySet())
 
-
-	local_file_hash_map.each {
-		// console.println("line in local hash map: " + it.key + ":" + it.value)
-	}
-
-	remote_plugins = (remote_file_hash_map.findAll { it.key.startsWith("plugins/") }).keySet()
+	// get list of plugins
+	def remote_plugins = (remote_file_hash_map.findAll { it.key.startsWith("plugins/") }).keySet()
+	fetch_plugins_by_name( local_file_hash_map, remote_plugins)
+	// get map of the rest of custom files (config and scripts)
 	remote_file_hash_map = remote_file_hash_map.findAll { !it.key.startsWith("plugins/") }
-
-
-	def local_plugins_dir = new File(local_config_dir.toString() + File.separator + "plugins")
-	remote_plugins.each {
-		console.println(it)
-		def downloaded = local_file_hash_map.containsKey(it)
-		if (!downloaded) {
-			console.println(remote_config_dir + it + " will be written to " + local_plugins_dir)
-			download_asset(remote_config_dir + it, local_plugins_dir)
-		}
-	}
-
-
-	// compare remote to local and get updated assets
-	remote_file_hash_map.each {
-		remote_asset_name = it.key
-		remote_asset_hash = it.value
-
-
-		def message
-		// if the remote asset is found in the assets folder
-		def downloaded = local_file_hash_map.containsKey(remote_asset_name)
-		if (downloaded) {
-			def local_asset_hash = local_file_hash_map.find{ it.key == remote_asset_name }?.value
-			if (local_asset_hash) {
-				if (local_asset_hash == remote_asset_hash) {
-					// message = "Remote asset ${remote_asset_name} hasn't changed, the local copy is up to date."
-					message = "..."
-				} else {
-					message = "Remote asset ${remote_asset_name} has been updated, a new download is needed."
-					// download
-					download_asset(remote_config_dir + remote_asset_name, local_config_dir)
-				}
-			}
-		} else {
-			// download
-			// check if it exists locally, if it does, then msg:
-			message = "Remote asset ${remote_asset_name} is new or had not been downloaded, will be downloaded now."
-			// download
-			download_asset(remote_config_dir + remote_asset_name, local_config_dir)
-		}
-
-		console.println(message)
-	}
-	console.println("Done!\n")
-}
-
-def delete_other_versions(urlstr_to_remote_asset, dest_dir) {
-
-	/*
-		this fucntion should delete the following jar files from directory <config_dir>\plugins
-		* okapiFiltersForOmegaT-1.6-m40-custom.jar
-		* okapiFiltersForOmegaT-1.7-1.40.0-SNAPSHOT.jar
-		* plugin-omt-package-1.4.1.jar
-		* plugin-omt-package-1.6.1.jar
-		(sample files for testing available here: https://capps.capstan.be/Files/plugins.zip)
-
-		The download_asset function should download (and overwrite):
-		* omegat-bidimarkers-0.2.0-all.jar
-		* okapiFiltersForOmegaT-1.6-m40-capstan.jar
-		* plugin-omt-package-1.6.3.jar
-		from https://cat.capstan.be/OmegaT/installer/plugins/
-	*/
-
-	URL url = new URL(urlstr_to_remote_asset)
-	remote_asset_name = FilenameUtils.getName(url.getPath()) // -> file.xml
-
-	def lib_name = remote_asset_name.minus(~/-\d+.*\.jar$/)
-	lib_to_delete = lib_name + "*.jar"
-	// console.println("\nI will delete " + lib_to_delete)
-	def files_to_delete = new FileNameFinder().getFileNames(dest_dir, lib_name + '*.jar' /* includes */, remote_asset_name /* excludes */)
-
-	files_to_delete.each { it -> // it is a path
-
-		console.println("***\nDELETE " + it)
-		def file = new File(it)
-
-		try {
-			ByteArrayOutputStream bos = new ByteArrayOutputStream(0);
-			FileOutputStream fos = new FileOutputStream(file);
-			// this will deplete the file, so it can be deleted afterwards by cmd
-			bos.writeTo(fos);
-			fos.close();
-			// System.gc()
-			// file.deleteOnExit() // me beating about the bush
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	//
-	// String folderPath = dest_dir
-	// new File(folderPath).eachFile (FileType.FILES) { file ->
-	// 		//Delete file if file name contains Jenkins
-	//    if (file.name.contains(lib_name)) file.delete()
-	// }
+	fetch_files_by_hash(   local_file_hash_map, remote_file_hash_map)
 
 }
 
-files = [ "omegat-bidimarkers-0.2.0-all.jar", "plugin-omt-package-1.6.3.jar", "okapiFiltersForOmegaT-1.6-m40-capstan.jar" ]
-// "okapiFiltersForOmegaT-1.6-m40-capstan.jar",
-
-
-//
-dest_dir = config_dir + "plugins"
-domain = "https://cat.capstan.be/OmegaT/custom/"
+message = []
+assert message.empty
+// constants
+digest = MessageDigest.getInstance("MD5") // not using def gives the variable global scope
+// timestamp = new Date().format("YYYYMMddHHmm")
+config_dir = StaticUtils.getConfigDir() // omegat internals // if
+plugins_dir = new File(config_dir.toString() + File.separator + "plugins")
+scripts_dir = new File(config_dir.toString() + File.separator + "scripts")
+// dest_dir = config_dir + "plugins"
 // def list_url = 			tb_domain + "list_contents.php"
+remote_config_dir = domain + "config/"
 hash_filename = "hash_list.txt"
-update_assets(domain, config_dir)
+// main function
+update_assets()
 
-files.each { file ->
-	url_str = domain + "plugins/" + file
-	// delete_other_versions(url_str, dest_dir)
-	// download_asset(url_str, dest_dir)
-}
-
+no_updates_msg = "All custom files were already up to date, nothing downloaded."
+// message.each { line -> console.println(line) }
+console.println( !message.empty ? message.join('\n') : no_updates_msg )
+console.println("Done!")
 
 // console.println("eventType: " + eventType)
 // console.println("Collecting garbage...")
